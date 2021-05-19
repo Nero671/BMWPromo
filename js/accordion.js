8bc12c55afc76ff5b5b8e4d94ca60bd0e103610f
Nero671 <stas.kachalko@mail.ru>
2021-05-19 22:51:08 +0300

    chore: add accordion | script

---
+++ js/accordion.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const featureLink = document.querySelectorAll('.feature__link');
+  const featureSub = document.querySelectorAll('.feature-sub');
+
+
+  featureLink.forEach((item, i) => {
+    item.addEventListener('click', () => {
+      if(item.classList.contains('feature__link_active')) {
+        item.classList.remove('feature__link_active');
+        featureSub[i].classList.add('hidden');
+      } else {
+        featureSub.forEach(item => item.classList.add('hidden'));
+        featureLink.forEach(item => item.classList.remove('feature__link_active'));
+
+        featureSub[i].classList.remove('hidden');
+        item.classList.add('feature__link_active');
+      }
+    })
+  })
+});
